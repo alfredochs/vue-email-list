0459e873694478360b05bcc1f471ad77bd008390
alfredochs <chanta.alfredo@gmail.com>
2021-11-09 14:41:45 +0100

    exercise done

--- js/script.js
+++ js/script.js
@@ -11,15 +11,19 @@ new Vue({
 
     },
     mounted() {
-        axios.get(this.url).then((ajaxResponse) => {
 
-            // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
-            // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
+        for (let index = 0; index < 10; index++) {
 
-            const rispotaUrl = ajaxResponse.data;
-            this.emaiDaUrl = rispotaUrl.response;
+            axios.get(this.url).then((ajaxResponse) => {
 
-            this.emailContainer.push(this.emaiDaUrl);
-        });
+                // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
+                // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
+
+                const rispotaUrl = ajaxResponse.data;
+                this.emaiDaUrl = rispotaUrl.response;
+
+                this.emailContainer.push(this.emaiDaUrl);
+            });
+        }
     }
 });
