d00d0c172d934cbb7561529c73b6edb8c8d383b4
alfredochs <chanta.alfredo@gmail.com>
2021-11-09 14:36:37 +0100

    1 email in array

---
+++ js/script.js
@@ -0,0 +1,25 @@
+Vue.config.devtools = true;
+new Vue({
+    el: "#root",
+    data: {
+        msj: "Benvenuto",
+        url: "https://flynn.boolean.careers/exercises/api/random/mail",
+        emailContainer: [],
+        emaiDaUrl: ""
+    },
+    methods: {
+
+    },
+    mounted() {
+        axios.get(this.url).then((ajaxResponse) => {
+
+            // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
+            // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
+
+            const rispotaUrl = ajaxResponse.data;
+            this.emaiDaUrl = rispotaUrl.response;
+
+            this.emailContainer.push(this.emaiDaUrl);
+        });
+    }
+});
