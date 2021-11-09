e65ac1de4c4af846e6505d08eb4afcae8c3e8be7
alfredochs <chanta.alfredo@gmail.com>
2021-11-09 15:30:06 +0100

    bonus done

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -6,24 +6,47 @@ new Vue({
         url: "https://flynn.boolean.careers/exercises/api/random/mail",
         emailContainer: [],
         emaiDaUrl: "",
+        contatore: 0
     },
     methods: {
+        generaEmail() {
+            let tempContainer = [];
 
+            for (let index = 0; index < 10; index++) {
+                axios.get(this.url).then((ajaxResponse) => {
+
+                    // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
+                    // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
+
+                    this.contatore++;
+                    const rispotaUrl = ajaxResponse.data;
+                    this.emaiDaUrl = rispotaUrl.response;
+
+                    tempContainer.push(this.emaiDaUrl);
+                    if (this.contatore === 10) {
+                        this.emailContainer = tempContainer;
+                        console.log(this.contatore);
+                    }
+
+                });
+            }
+
+        }
     },
     mounted() {
 
-        for (let index = 0; index < 10; index++) {
+        // for (let index = 0; index < 10; index++) {
 
-            axios.get(this.url).then((ajaxResponse) => {
+        //     axios.get(this.url).then((ajaxResponse) => {
 
-                // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
-                // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
+        //         // ajaxResponse = risposta di axios alla chiamata ajax. E' un oggetto che contiene vari dati,
+        //         // tra cui la chiave "data", che conterrà la risposta ricevuta dal server.
 
-                const rispotaUrl = ajaxResponse.data;
-                this.emaiDaUrl = rispotaUrl.response;
+        //         const rispotaUrl = ajaxResponse.data;
+        //         this.emaiDaUrl = rispotaUrl.response;
 
-                this.emailContainer.push(this.emaiDaUrl);
-            });
-        }
+        //         this.emailContainer.push(this.emaiDaUrl);
+        //     });
+        // }
     }
 });
